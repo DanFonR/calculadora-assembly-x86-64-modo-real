e908d1592221c06850e95f52d955ac60b697fe00
regimarnegreiros <regimar.negreiros@gmail.com>
2025-03-09 15:13:03 -0300

    Função que coleta a opção do usuario.

--- calculadora.asm
+++ calculadora.asm
@@ -3,9 +3,15 @@ section .data
              db '1 - Soma', 10
              db '2 - Subtracao', 10
              db '3 - Multiplicacao', 10
-             db '4 - Divisao', 10, 0
+             db '4 - Divisao', 10
+             db 'Opcao: ', 0
+
+    opcao_escolhida_msg db 'Opcao escolhida: ', 0
 
     resultado_msg db 'O resultado da operacao e: ', 0
+
+section .bss
+    opcao resb 2                  ; resb 2 para armazenar o numero e o \n quando o usuario apertar enter
 
 section .text
     global _start
@@ -13,6 +19,14 @@ section .text
 _start:
     mov rdi, menu_msg             ; Passa o endereço de menu_mgs para o parâmetro (rdi)
     call print_string             ; Chama a função print_message
+
+    call coletar_opcao            ; Chama a função que coleta a opção
+    
+    mov rdi, opcao_escolhida_msg
+    call print_string
+    
+    mov rdi, opcao
+    call print_string
 
     mov rdi, resultado_msg
     call print_string
@@ -41,6 +55,14 @@ print_string:
         mov rax, 1                ; Número da chamada sys_write
         syscall                   ; Chamada de sistema para escrever
         ret                       ; Retorna para onde a funcao foi chamada
+
+coletar_opcao:
+    mov rdi, 0                    ; Leitura da entrada padrão (0 = STDIN)
+    mov rsi, opcao                ; Ponteiro para a variável opcao
+    mov rdx, 2                    ; Número de bytes a serem lidos (apenas 1 caractere para a opção)
+    mov rax, 0                    ; Número da chamada sys_read
+    syscall                       ; Chama o sistema para ler a entrada
+    ret
 
 ; Funcao finalizar_programa: Finaliza o programa e retorna 0 (sucesso)
 finalizar_programa:
